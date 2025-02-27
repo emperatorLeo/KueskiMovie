f5f0a403d685731c7244a73f6f6f802d57edc6ae
emperatorLeo <leo_svxy@hotmail.com>
2025-02-27 16:40:00 -0500

    fixing type

--- listMovies/src/main/java/com/example/listmovies/presentation/component/NoInternetComponent.kt
+++ listMovies/src/main/java/com/example/listmovies/presentation/component/NoInternetComponent.kt
@@ -30,7 +30,7 @@ fun NoInternetComponent() {
     ) {
         Image(
             modifier = Modifier.size(Dimen200dp),
-            painter = painterResource(R.drawable.ic_error_conection),
+            painter = painterResource(R.drawable.ic_error_connection),
             contentDescription = ""
         )
 
